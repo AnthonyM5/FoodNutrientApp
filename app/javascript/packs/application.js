12acd92584a21f0454fc4140f1d7344df34507a5
antimonysayz <antimonysayz@gmail.com>
2020-08-10 23:57:32 -0400

    Log Out Works

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -10,6 +10,7 @@ require("channels")
 
 
 //= require jquery3
+//= require jquery_ujs
 //= require popper
 //= require bootstrap
 // Uncomment to copy all static images under ../images to the output folder and reference
