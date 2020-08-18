8e4b6c5672c137a4451247ddc4db668a791fb4fd
antimonysayz <antimonysayz@gmail.com>
2020-08-18 16:33:41 -0400

    comments validates user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,9 @@
 class ApplicationController < ActionController::Base
 
     def authorized
-        unless !!current_user
-            flash[:errors]= ["Please Login"]
-            return redirect_to '/login'
+        unless current_user
+            flash[:errors] = ["Please Login"]
+            return redirect_to new_user_session_path
         end
     end
     
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,6 +4,7 @@ class CommentsController < ApplicationController
     def create
         @meal = Meal.find_by(id: params[:meal_id])
         comment = @meal.comments.create(comments_params)
+        redirect_to meal_path(@meal)
     end
 
     def comments_params
