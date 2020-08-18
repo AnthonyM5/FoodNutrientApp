6853a52bb54c182b67bf1c926b90b35ac29c0825
antimonysayz <antimonysayz@gmail.com>
2020-08-18 16:09:05 -0400

    adding comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
+
+    def authorized
+        unless !!current_user
+            flash[:errors]= ["Please Login"]
+            return redirect_to '/login'
+        end
+    end
     
 end
--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,13 @@
 class CommentsController < ApplicationController
+    before_action :authorized 
+
     def create
-        comment = @meal.comment.create(comment_params)
+        @meal = Meal.find_by(id: params[:meal_id])
+        comment = @meal.comments.create(comments_params)
     end
 
     def comments_params
-        params.require(:meal).permit(:content, :user_id)
+        params[:comment][:user_id] = current_user.id
+        params.require(:comment).permit(:content, :user_id)
     end
 end
