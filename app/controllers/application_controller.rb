641d13f247f3dac2d5fc829671a76d54acf765e4
antimonysayz <antimonysayz@gmail.com>
2020-08-20 22:15:37 -0400

    using validations for comment creation

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     def authorized
         unless !!current_user
             flash[:errors]= ["Please Login"]
-            return redirect_to '/login'
+            return redirect_to new_user_session_path	
         end
     end
     
